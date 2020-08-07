a2a4628ce0c3f261c6d20162e677538b6dfe2546
Liz Ruelas <liz.ruelas@tcs.com>
2020-08-06 19:31:30 -0500

    init

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        margin: 10,
+        nav: true,
+        navText:["<div class='nav-btn prev-slide'><img src='assets/img/arrow-left.png'></div>","<div class='nav-btn next-slide'><img src='assets/img/arrow-right.png'></div>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+  });
